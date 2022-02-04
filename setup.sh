a41b68f2f6f5dd52da14ad54f6b47b90edfe0037
Pete Keen <pete@petekeen.net>
2022-02-03 20:57:58 -0500

    bump

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/ash
+
+cat > /etc/apk/repositories << EOF; $(echo)
+
+http://dl-cdn.alpinelinux.org/alpine/v$(cat /etc/alpine-release | cut -d'.' -f1,2)/main
+http://dl-cdn.alpinelinux.org/alpine/v$(cat /etc/alpine-release | cut -d'.' -f1,2)/community
+http://dl-cdn.alpinelinux.org/alpine/edge/testing
+
+EOF
+
+apk update
+
+apk install docker tailscale
+
+rc-service docker start
+rc-update add docker
+
+rc-service tailscale start
+rc-update add tailscale
+
+
+tailscale up --advertise-tags tag:server,tag:zwave
+
+docker network create -d bridge -o com.docker.network.bridge.host_binding_ipv4=$(tailscale ip | head -n1) tailnet
+
